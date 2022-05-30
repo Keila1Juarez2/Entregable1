8359728f2c42183cb766279a2baffbdc7ecf77fa
Keila Juarez Esparza <juarez.zuri.721@gmail.com>
2022-05-30 00:02:43 -0500

    progress

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const QuoteBox = () => {
+
+  const colorRandom = [
+    '#6CADB4', '#9F6AE9', '#E8B1A5', '#92B7D9', '#326C5F', '#788CD9', '#BA8DCF', '#127F78', '#2CBCFF', '#55FBF3', '#4374B9'
+] 
+
+  return (
+    <div>QuoteBox</div>
+  )
+}
+
+export default QuoteBox
